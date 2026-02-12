329cd63770436849593d4faec4362e9654736299
thanos1983 <7160603+thanos1983@users.noreply.github.com>
2026-02-12 22:53:40 +0100

    Updating code by adding vault for security.

--- IaC/AkS/tf/provider.tf
+++ IaC/AkS/tf/provider.tf
@@ -1,7 +1,4 @@
-provider "cloudflare" {
-  email   = local.decoded_vault_yaml.cloudflare.email
-  api_key = local.decoded_vault_yaml.cloudflare.api_key
-}
+provider "ansible" {}
 
 provider "azurerm" {
   features {
@@ -13,9 +10,21 @@ provider "azurerm" {
   subscription_id                 = var.ARM_SUBSCRIPTION_ID # set as env variable
 }
 
+provider "cloudflare" {
+  email   = local.decoded_vault_yaml.cloudflare.email
+  api_key = local.decoded_vault_yaml.cloudflare.api_key
+}
+
 provider "kubectl" {
   load_config_file       = var.load_config_file
   apply_retry_count      = var.apply_retry_count
+  host                   = module.aks_project_aks_cluster.kube_admin_config[0].host
+  client_key             = base64decode(module.aks_project_aks_cluster.kube_admin_config[0].client_key)
+  client_certificate     = base64decode(module.aks_project_aks_cluster.kube_admin_config[0].client_certificate)
+  cluster_ca_certificate = base64decode(module.aks_project_aks_cluster.kube_admin_config[0].cluster_ca_certificate)
+}
+
+provider "kubernetes" {
   host                   = module.aks_project_aks_cluster.kube_admin_config[0].host
   client_key             = base64decode(module.aks_project_aks_cluster.kube_admin_config[0].client_key)
   client_certificate     = base64decode(module.aks_project_aks_cluster.kube_admin_config[0].client_certificate)
@@ -29,11 +38,4 @@ provider "helm" {
     client_certificate     = base64decode(module.aks_project_aks_cluster.kube_admin_config[0].client_certificate)
     cluster_ca_certificate = base64decode(module.aks_project_aks_cluster.kube_admin_config[0].cluster_ca_certificate)
   }
-}
-
-provider "kubernetes" {
-  host                   = module.aks_project_aks_cluster.kube_admin_config[0].host
-  client_key             = base64decode(module.aks_project_aks_cluster.kube_admin_config[0].client_key)
-  client_certificate     = base64decode(module.aks_project_aks_cluster.kube_admin_config[0].client_certificate)
-  cluster_ca_certificate = base64decode(module.aks_project_aks_cluster.kube_admin_config[0].cluster_ca_certificate)
 }
